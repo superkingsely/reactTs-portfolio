72950148dcab0f1bc40798f017f7a6bbb1695f76
unknown <paulyriksifeanyi@gmail.com>
2023-08-15 17:49:40 +0100

    ookay

---
+++ OneDrive/Desktop/JS project file/js practice2/index.js
@@ -0,0 +1,25 @@
+// dom
+const h1=document.createElement('h1')
+h1.innerText='welcome to my dom class'
+h1.style.backgroundColor='red'
+document.body.appendChild(h1)
+
+const p=document.createElement('p')
+p.innerText='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dignissimos illum exercitationem ratione nihil? Eius dolorem possimus iusto commodi maxime deserunt sit cupiditate magni sunt. Reprehenderit consequatur autem fugit pariatur?'
+
+document.body.append(p)
+    
+
+const h2=document.createElement('h2')
+h2.innerText='Halle Berry is good'
+document.body.append(h2)
+
+const a=document.createElement('a')
+a.innerText='click me'
+a.href='https://jiji.ng'
+document.body.append(a)
+
+const body=document.querySelector('body')
+body.style.backgroundColor=' #00ffbb'
+
+

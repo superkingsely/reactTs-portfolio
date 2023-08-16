9b5407a4698296d7a26577bcee2b2a1070491db7
unknown <paulyriksifeanyi@gmail.com>
2023-08-16 19:05:56 +0100

    okay

--- OneDrive/Desktop/JS project file/js practice2/index.js
+++ OneDrive/Desktop/JS project file/js practice2/index.js
@@ -1,20 +1,45 @@
+// const app=document.createElement('div')
+// app.style.backgroundColor='grey'
+// app.style.minHeight='100vh'
+// document.body.appendChild(app)
+
+// // navbar
+// const navbar=document.createElement('nav')
+// navbar.style.backgroundColor='blue'
+// navbar.style.minHeight='10vh'
+// app.appendChild(navbar)
+// //main
+// const main=document.createElement('main')
+// main.style.backgroundColor='aqua'
+// main.style.minHeight='80vh'
+// app.appendChild(main)
+// // footer
+// const footer=document.createElement('nav')
+// footer.style.backgroundColor='blue'
+// footer.style.minHeight='10vh'
+// app.appendChild(footer)
+
+
+
+
+
+
 const app=document.createElement('div')
-app.style.backgroundColor='grey'
-app.style.minHeight='100vh'
+app.style.backgroundColor=('red')
+app.style.minHeight=('100vh')
 document.body.appendChild(app)
 
-// navbar
 const navbar=document.createElement('nav')
-navbar.style.backgroundColor='blue'
-navbar.style.minHeight='10vh'
+navbar.style.backgroundColor=('blue')
+navbar.style.minHeight=('10vh')
 app.appendChild(navbar)
-//main
+
 const main=document.createElement('main')
-main.style.backgroundColor='aqua'
-main.style.minHeight='80vh'
+main.style.backgroundColor=('green')
+main.style.minHeight=('80vh')
 app.appendChild(main)
-// footer
-const footer=document.createElement('nav')
-footer.style.backgroundColor='blue'
-footer.style.minHeight='10vh'
+
+const footer=document.createElement('footer')
+footer.style.backgroundColor=('blue')
+footer.style.minHeight=('10vh')
 app.appendChild(footer)
